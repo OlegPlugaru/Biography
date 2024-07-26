c6be34f5156e3715691482932b8ebda038dc2e58
OlegPlugaru <olegplugaru.md@gmail.com>
2024-07-26 20:07:17 +0300

    Initial Commit

---
+++ Biography/BiographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiographyApp.swift
+//  Biography
+//
+//  Created by Oleg Plugaru on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BiographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiographyTests/BiographyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiographyTests.swift
+//  BiographyTests
+//
+//  Created by Oleg Plugaru on 26.07.2024.
+//
+
+import Testing
+@testable import Biography
+
+struct BiographyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
